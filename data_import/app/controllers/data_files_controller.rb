dcd83ebe3c8a1cdfd9a86253438475f730d3bd71
Chris Flipse <cflipse@gmail.com>
2011-10-18 19:03:24 -0400

    fill in create method in controller

--- data_import/app/controllers/data_files_controller.rb
+++ data_import/app/controllers/data_files_controller.rb
@@ -1,5 +1,18 @@
+require 'data_persistence'
 class DataFilesController < ApplicationController
   def index
+  end
+
+  def create
+    importer = DataImporter.new(params[:data].path)
+
+    if importer.save
+      flash[:notice] = "Successfully imported data.  Gross Revenue: $#{importer.gross_revenue}"
+    else
+      flash[:error] = "There was a problem importing your file"
+    end
+
+    redirect_to data_files_path
   end
 
 end
--- data_import/spec/controllers/data_files_controller_spec.rb
+++ data_import/spec/controllers/data_files_controller_spec.rb
@@ -9,4 +9,46 @@ describe DataFilesController do
     end
   end
 
+  describe "POST create" do
+    let(:path) { Rails.root.join("spec/data/example_input.tab") }
+    let(:file) {  Rack::Test::UploadedFile.new(path) }
+
+    let(:importer) { stub("data importer", :save => true, :gross_revenue => 50) }
+    before { DataImporter.stub(:new => importer) }
+    it "builds and saves a new DataImporter" do
+      importer.should_receive :save
+      post :create, :data => file
+    end
+
+    it "passes the file on to the importer"  do
+      DataImporter.should_receive(:new).with(file.path)
+      post :create, :data => file
+    end
+
+    it "redirects to the index page" do
+      post :create, :data => file
+      response.should redirect_to data_files_path
+    end
+
+    context "successfully" do
+      before { importer.stub(:save => true, :gross_revenue => 100.0) }
+
+      it "sets a success flash with revenue numbers" do
+        post :create, :data => file
+        flash[:notice].should =~ /Gross Revenue: \$100\.0/
+      end
+
+
+    end
+
+    context "unsuccessfully" do
+      before { importer.stub(:save => false) }
+      it "sets an error flash" do
+        post :create, :data => file
+        flash[:error].should be_present
+      end
+    end
+
+  end
+
 end

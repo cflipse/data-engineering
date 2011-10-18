3ac966865c0bf3421b88510af9eff81b06af73a7
Chris Flipse <cflipse@gmail.com>
2011-10-18 17:58:27 -0400

    generated customer model

---
+++ data_import/spec/models/customer_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Customer do
+  it { should validate_presence_of :name }
+
+  describe "uniqueness" do
+    before { Customer.create :name => "Bob" }
+    it { should validate_uniqueness_of :name }
+  end
+end

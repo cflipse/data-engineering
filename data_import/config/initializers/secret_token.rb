76ba90f46403d1a7b4b772883839a05db5044afd
Chris Flipse <cflipse@gmail.com>
2011-10-18 16:55:54 -0400

    generated new rails app

---
+++ data_import/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataImport::Application.config.secret_token = 'bd805013d787a6492a3a3a44bd12e397a4055bd37edc1e867aaed9de9285e4bca0c95c3c0367e1589df273f0eb34cba4d8560b3d4e420eb765365bedc5e65023'
